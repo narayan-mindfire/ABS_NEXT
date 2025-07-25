4d3de8296941130bf201cfab2a26f1b7d386c104
Narayan Pradhan <narayanp@mindfiresolutions.com>
2025-07-25 13:01:52 +0530

    fix: update server action error 3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx", "server.ts"],
   async headers() {
     return [
